4524bf0cd7124ce7623b9208cb4dd5278c886993
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2025-04-09 19:19:21 +0200

    Dark mode fix for HTML tables

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,24 @@ autodoc_default_options = {
     "members": True,
 }
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# httpdomain settings
+http_strict_mode = True
+http_index_localname = "HTTP Routing Table"
+http_index_shortname = 'api'
+
 # Theme settings
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
